63f8fbca6583d735c3de46b5609fd426d4e67a93
MistletoeXW <15178931042@qq.com>
2018-07-29 16:23:54 +0800

    完成孰知网的各条新闻的url获取

--- SHUmassageScrapy/spiders/shuzhi.py
+++ SHUmassageScrapy/spiders/shuzhi.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
 import requests
 import json
 
@@ -15,7 +16,40 @@ class ShuzhiSpider(scrapy.Spider):
     }
     session = requests.session()
 
-    def parse(self, response):
+    # def get_total_url(self, response):
+    #     """
+    #     提取出通知公告页面更多项的url
+    #     """
+    #     Total_urls = response.css(".MoreList a::attr(href)").extract()  #获取通知公告栏url
+    #     News_urls = str(response.url) + Total_urls[0]
+    #     yield scrapy.Request(News_urls, headers=self.header, callback=self.get_sort_url)
+    #
+    # def get_sort_url(self, response):
+    #     """
+    #     提取出各个边栏的url以便获取具体新闻的url
+    #     """
+    #     urls = response.css(".Gonggaotag li a::attr(href)").extract()
+    #     Sorts_url = []
+    #     for url in urls:
+    #         Sorts_url.append("http://sz.shu.edu.cn"+url)
+    #     for Sort_url in Sorts_url:
+    #         yield scrapy.Request(Sort_url, headers=self.header, callback=self.get_news_url)
+
+    def get_news_url(self, response):
+        """
+        使用深度优先的方法，首先提取出所有的新闻url再进一步进行内容提取
+        """
+        news_url = response.css(".Tongzhiul a::attr(href)").extract()
+        urls = []
+        for new_url in news_url:
+            if not new_url.startswith("http"):
+                url = "http://sz.shu.edu.cn"+new_url
+                urls.append(url)
+            else:
+                urls.append(new_url)
+        pass
+
+    def parse_detail(self, response):
         pass
 
     def start_requests(self):
@@ -40,4 +74,4 @@ class ShuzhiSpider(scrapy.Spider):
         text_json = json.loads(response.text)
         if "message" in text_json and text_json["message"] == "成功":
             for url in self.start_urls:
-                yield scrapy.Request(url, dont_filter=True, headers=self.header)
+                yield scrapy.Request(url, dont_filter=True, headers=self.header,callback=self.get_news_url)
